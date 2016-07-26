8933ae02a7b98561d9c2abeab1cb56439ae28039
David Moncayo Torres <d.alonsomt@gmail.com>
2016-07-25 19:19:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/davidmoncayotorres/schoolapp/SchoolMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.davidmoncayotorres.schoolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SchoolMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_main);
+    }
+}
